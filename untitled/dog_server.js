27ecf793827f97cea94a6ddce4c25acb37761945
Rahul Shrestha <rahul.shrestha@selu.edu>
2016-02-23 10:25:34 -0600

    new server and models
    additional servers and reorganizing routes

--- untitled/dog_server.js
+++ untitled/dog_server.js
@@ -1,17 +1,20 @@
+/**
+ * Created by rahulshrestha on 2/23/16.
+ */
 var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/cats');
+mongoose.connect('mongodb://localhost:27017/dogs');
 
 app.use (bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-var cats = require('./routes/cat_routes.js')(app);
+var dogRoutes = require('./routes/dog_routes.js')(app);
 
 var server = app.listen(3000, function(){
     console.log('running at 3000');
--- untitled/routes/cat_routes.js
+++ untitled/routes/cat_routes.js
@@ -1,5 +1,5 @@
 var _ = require('lodash');
-var Cat = require('./cat_model.js');
+var Cat = require('./../cat_model.js');
 
 module.exports = function(app) {
 
